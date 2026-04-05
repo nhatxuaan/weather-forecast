74d13ca4f4b667d3237ca34b10d119b67eb0c093
Nhat Xuan <nhatxuaan@gmail.com>
2026-04-05 19:33:25 +0700

    weather-object

---
+++ weather-object.js
@@ -0,0 +1,9 @@
+let weather ={
+     temperature: 40,
+     conditions: "rainy"
+};
+//Log the current temperature to the console.
+console.log(weather.temperature);
+console.log(weather.conditions);
+//Use string template literals to log a sentence about the weather console. (Example: "It's 64 degrees F and sunny.")
+console.log(`It's ${weather.temperature} degrees F and ${weather.conditions}.`);
