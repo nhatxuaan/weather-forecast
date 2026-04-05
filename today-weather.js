11deb41a9f184ab5fe09faf24e8769bc7fa9d512
Nhat Xuan <nhatxuaan@gmail.com>
2026-04-05 19:06:02 +0700

    today's weather

---
+++ today-weather.js
@@ -0,0 +1,12 @@
+let temperature = 50;
+let conditions = 'sunny';
+let sunriseTime='7:01 AM';
+let sunsetTime ='6:02 PM';
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+console.log(`It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`);
+
